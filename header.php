5084367269e2fa17bdd185c20901fe80c41ad0e9
amtrak24 <jacobsjmd01@comcast.net>
2015-03-29 01:03:04 -0700

    Adds basic website

---
+++ header.php
@@ -0,0 +1,48 @@
+<?php
+	echo 
+
+		<!-- Header -->
+			<div id="header">
+
+				<!-- Logo -->
+					<h1><a href="index.html" id="logo"><em>San Francisco</em> Noise Watch</a></h1>
+
+				<!-- Nav -->
+					<nav id="nav">
+						 <ul>
+							<li class="current"><a href="index.html">Home</a></li>
+							<!--
+							<li>
+								<a href="">Dropdown</a>
+								<ul>
+									<li><a href="#">Lorem dolor</a></li>
+									<li><a href="#">Magna phasellus</a></li>
+									<li><a href="#">Etiam sed tempus</a></li>
+									<li>
+										<a href="">Submenu</a>
+										<ul>
+											<li><a href="#">Lorem dolor</a></li>
+											<li><a href="#">Phasellus magna</a></li>
+											<li><a href="#">Magna phasellus</a></li>
+											<li><a href="#">Etiam nisl</a></li>
+											<li><a href="#">Veroeros feugiat</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Veroeros feugiat</a></li>
+								</ul>
+							</li> -->
+							<li><a href="impacts.html">Impacts</a></li>
+							<li><a href="laws.html">Laws</a></li>
+							<li><a href="noise.html">Noise in SF</a></li>
+							<li><a href="report.html">Report</a></li>
+							<li><a href="resources.html">Resources</a></li>
+						</ul>
+					</nav>
+
+			</div>;
+
+
+
+
+
+?>
